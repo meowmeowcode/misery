497c17d4cf3d3553a2a2e91a3e119440b1204720
Anton Evdokimov <meowmeowcode@gmail.com>
2022-04-18 21:54:18 +0300

    add a repository implementation for postgresql

---
+++ depression/pg_repo.py
@@ -0,0 +1,230 @@
+import dataclasses
+import typing
+from abc import abstractmethod
+from enum import Enum
+from typing import Any
+from typing import Generic
+from typing import Iterable
+from typing import Optional
+from typing import Protocol
+from typing import Sequence
+from typing import TypeVar
+
+from asyncpg import Connection  # type: ignore
+from pypika import Order  # type: ignore
+from pypika import PostgreSQLQuery  # type: ignore
+from pypika import Table  # type: ignore
+from pypika import functions as fn  # type: ignore
+from pypika.terms import BasicCriterion  # type: ignore
+from pypika.terms import Term  # type: ignore
+from pypika.enums import Comparator  # type: ignore
+
+from .core import F
+from .core import FilterType
+from .core import NotFound
+
+
+T = TypeVar("T")
+
+
+class _PostgreSQLMatching(Comparator):
+    regexp = " ~ "
+    not_regexp = " !~ "
+    iregexp = " ~* "
+    not_iregexp = " !~* "
+
+
+class _Regexp(BasicCriterion):
+    def __init__(self, term: Term, expr: str) -> None:
+        super().__init__(_PostgreSQLMatching.regexp, term, term.wrap_constant(expr))
+
+
+class _NotRegexp(BasicCriterion):
+    def __init__(self, term: Term, expr: str) -> None:
+        super().__init__(_PostgreSQLMatching.not_regexp, term, term.wrap_constant(expr))
+
+
+class _IRegexp(BasicCriterion):
+    def __init__(self, term: Term, expr: str) -> None:
+        super().__init__(_PostgreSQLMatching.iregexp, term, term.wrap_constant(expr))
+
+
+class _NotIRegexp(BasicCriterion):
+    def __init__(self, term: Term, expr: str) -> None:
+        super().__init__(
+            _PostgreSQLMatching.not_iregexp, term, term.wrap_constant(expr)
+        )
+
+
+class PGRepo(Generic[T]):
+    id_field = "id"
+
+    @property
+    @abstractmethod
+    def table_name(self) -> str:
+        ...
+
+    def __init__(self, conn: Connection) -> None:
+        self._conn = conn
+
+    @property
+    def table(self) -> Table:
+        return Table(self.table_name)
+
+    @property
+    def query(self) -> PostgreSQLQuery:
+        return PostgreSQLQuery.from_(self.table).select("*")
+
+    @property
+    def _entity_type(self) -> type:
+        return typing.get_args(self.__orig_bases__[0])[0]  # type: ignore
+
+    def load(self, record: dict) -> T:
+        return self._entity_type(**record)
+
+    def dump(self, entity: T) -> dict:
+        return dataclasses.asdict(entity)
+
+    async def execute(self, query: PostgreSQLQuery) -> None:
+        await self._conn.execute(str(query))
+
+    async def fetch_one(self, query: PostgreSQLQuery) -> T:
+        data = await self._conn.fetchrow(str(query))
+
+        if data is None:
+            raise NotFound
+
+        return self.load(dict(data))
+
+    async def fetch_many(self, query: PostgreSQLQuery) -> Iterable[T]:
+        records = await self._conn.fetch(str(query))
+        return map(lambda x: self.load(dict(x)), records)
+
+    async def fetch_value(self, query: PostgreSQLQuery) -> Any:
+        return await self._conn.fetchval(str(query))
+
+    async def add(self, entity: T) -> None:
+        data = self.dump(entity)
+        query = (
+            PostgreSQLQuery.into(self.table)
+            .columns(*data.keys())
+            .insert(*data.values())
+        )
+        await self.execute(query)
+
+    async def get(self, **kwargs) -> T:
+        query = self.query.limit(1)
+
+        for k, v in kwargs.items():
+            query = query.where(self.table[k] == v)
+
+        return await self.fetch_one(query)
+
+    async def get_many(
+        self,
+        filter_: Sequence[F] = (),
+        order: Sequence[str] = (),
+        limit: Optional[int] = None,
+        page: int = 1,
+    ) -> Iterable[T]:
+        query = self.query
+
+        if limit is not None:
+            query = query.limit(limit).offset((page - 1) * limit)
+
+        for field in order:
+            if field.startswith("-"):
+                query = query.orderby(field[1:], order=Order.desc)
+            else:
+                query = query.orderby(field)
+
+        for f in filter_:
+            column = self.table[f.field]
+
+            if f.type == FilterType.EQ:
+                criterion = column == f.value
+            elif f.type == FilterType.NEQ:
+                criterion = column != f.value
+            elif f.type == FilterType.LT:
+                criterion = column < f.value
+            elif f.type == FilterType.GT:
+                criterion = column > f.value
+            elif f.type == FilterType.LTE:
+                criterion = column <= f.value
+            elif f.type == FilterType.GTE:
+                criterion = column >= f.value
+            elif f.type == FilterType.STARTSWITH:
+                criterion = column.like(f"{f.value}%")
+            elif f.type == FilterType.NSTARTSWITH:
+                criterion = column.not_like(f"{f.value}%")
+            elif f.type == FilterType.ENDSWITH:
+                criterion = column.like(f"%{f.value}")
+            elif f.type == FilterType.NENDSWITH:
+                criterion = column.not_like(f"%{f.value}")
+            elif f.type == FilterType.ISTARTSWITH:
+                criterion = column.ilike(f"{f.value}%")
+            elif f.type == FilterType.NISTARTSWITH:
+                criterion = column.not_ilike(f"{f.value}%")
+            elif f.type == FilterType.IENDSWITH:
+                criterion = column.ilike(f"%{f.value}")
+            elif f.type == FilterType.NIENDSWITH:
+                criterion = column.not_ilike(f"%{f.value}")
+            elif f.type == FilterType.CONTAINS:
+                criterion = column.like(f"%{f.value}%")
+            elif f.type == FilterType.NCONTAINS:
+                criterion = column.not_like(f"%{f.value}%")
+            elif f.type == FilterType.ICONTAINS:
+                criterion = column.ilike(f"%{f.value}%")
+            elif f.type == FilterType.NICONTAINS:
+                criterion = column.not_ilike(f"%{f.value}%")
+            elif f.type == FilterType.IN:
+                criterion = column.isin(f.value)
+            elif f.type == FilterType.NIN:
+                criterion = column.notin(f.value)
+            elif f.type == FilterType.MATCHES:
+                criterion = _Regexp(column, f.value)
+            elif f.type == FilterType.NMATCHES:
+                criterion = _NotRegexp(column, f.value)
+            elif f.type == FilterType.IMATCHES:
+                criterion = _IRegexp(column, f.value)
+            elif f.type == FilterType.NIMATCHES:
+                criterion = _NotIRegexp(column, f.value)
+
+            query = query.where(criterion)
+
+        return await self.fetch_many(query)
+
+    async def update(self, entity: T) -> None:
+        query = PostgreSQLQuery.update(self.table)
+        record = self.dump(entity)
+
+        for k, v in record.items():
+            query = query.set(k, v)
+
+        query.where(self.table[self.id_field] == getattr(entity, self.id_field))
+        await self.execute(query)
+
+    async def delete(self, **kwargs) -> None:
+        query = PostgreSQLQuery.from_(self.table).delete()
+
+        for k, v in kwargs.items():
+            query = query.where(self.table[k] == v)
+
+        await self.execute(query)
+
+    async def exists(self, **kwargs) -> bool:
+        query = PostgreSQLQuery.from_(self.table).select(1).limit(1)
+
+        for k, v in kwargs.items():
+            query = query.where(self.table[k] == v)
+
+        data = await self.fetch_value(query)
+        return data is not None
+
+    async def clean(self) -> None:
+        query = PostgreSQLQuery.from_(self.table).delete()
+        await self.execute(query)
+
+    async def count(self) -> int:
+        query = PostgreSQLQuery.from_(self.table).select(fn.Count("*"))
+        return await self.fetch_value(query)
---
+++ tests/test_pg_repo.py
@@ -0,0 +1,250 @@
+from __future__ import annotations
+
+import asyncio
+from dataclasses import dataclass
+from typing import AsyncGenerator
+from typing import Sequence
+from typing import Union
+
+import asyncpg  # type: ignore
+import pytest  # type: ignore
+from asyncpg import Connection  # type: ignore
+
+from depression.core import Repo
+from depression.pg_repo import PGRepo
+from depression import F
+
+from .base import (
+    Symptom,
+    SymptomType,
+    SymptomsRepo,
+)
+
+
+class SymptomsPGRepo(PGRepo[Symptom]):
+    table_name = "symptoms"
+
+    def dump(self, entity: Symptom) -> dict:
+        record = super().dump(entity)
+        record["type"] = entity.type.name
+        return record
+
+    def load(self, record: dict) -> Symptom:
+        record["type"] = SymptomType[record["type"]]
+        return super().load(record)
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session")
+def conn_str() -> str:
+    return "postgresql://depression:depression@localhost/depression"
+
+
+@pytest.fixture(scope="session")
+async def conn(conn_str: str) -> Connection:
+    return await asyncpg.connect(conn_str)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def db_schema(conn: Connection) -> AsyncGenerator:
+    await conn.execute(
+        """
+        CREATE TABLE symptoms (
+            id integer PRIMARY KEY,
+            name text NOT NULL UNIQUE,
+            type TEXT NOT NULL
+        )
+    """
+    )
+
+    yield
+
+    await conn.execute(f"drop table symptoms")
+
+
+@pytest.fixture(autouse=True)
+async def clean_db(db_schema: None, conn: Connection) -> None:
+    await conn.execute(f"TRUNCATE TABLE symptoms")
+
+
+@pytest.fixture
+def symptoms_repo(conn: Connection) -> SymptomsPGRepo:
+    return SymptomsPGRepo(conn)
+
+
+async def test_get(symptoms_repo: SymptomsRepo, hopelessness: Symptom) -> None:
+    s = await symptoms_repo.get(id=hopelessness.id)
+    assert s == hopelessness
+
+
+async def test_get_many(
+    symptoms_repo: SymptomsRepo, hopelessness: Symptom, helplessness: Symptom
+) -> None:
+    symptoms = list(await symptoms_repo.get_many())
+    assert symptoms == [hopelessness, helplessness]
+
+
+@pytest.mark.parametrize(
+    "filter_, names",
+    [
+        ([F.eq("name", "Insomnia")], ["Insomnia"]),
+        ([F.neq("name", "Insomnia")], ["Hopelessness", "Helplessness", "Constipation"]),
+        ([F.contains("name", "les")], ["Hopelessness", "Helplessness"]),
+        ([F.contains("name", "LES")], []),
+        ([F.ncontains("name", "les")], ["Insomnia", "Constipation"]),
+        (
+            [F.ncontains("name", "LES")],
+            ["Hopelessness", "Helplessness", "Insomnia", "Constipation"],
+        ),
+        ([F.icontains("name", "LES")], ["Hopelessness", "Helplessness"]),
+        ([F.nicontains("name", "LES")], ["Insomnia", "Constipation"]),
+        ([F.startswith("name", "H")], ["Hopelessness", "Helplessness"]),
+        ([F.startswith("name", "h")], []),
+        ([F.nstartswith("name", "H")], ["Insomnia", "Constipation"]),
+        (
+            [F.nstartswith("name", "h")],
+            ["Hopelessness", "Helplessness", "Insomnia", "Constipation"],
+        ),
+        ([F.istartswith("name", "h")], ["Hopelessness", "Helplessness"]),
+        ([F.nistartswith("name", "h")], ["Insomnia", "Constipation"]),
+        ([F.endswith("name", "ness")], ["Hopelessness", "Helplessness"]),
+        ([F.endswith("name", "NESS")], []),
+        ([F.nendswith("name", "ness")], ["Insomnia", "Constipation"]),
+        (
+            [F.nendswith("name", "NESS")],
+            ["Hopelessness", "Helplessness", "Insomnia", "Constipation"],
+        ),
+        ([F.iendswith("name", "NESS")], ["Hopelessness", "Helplessness"]),
+        ([F.niendswith("name", "NESS")], ["Insomnia", "Constipation"]),
+        ([F.matches("name", r"^H.*s$")], ["Hopelessness", "Helplessness"]),
+        ([F.matches("name", r"^h.*s$")], []),
+        ([F.nmatches("name", r"^H.*s$")], ["Insomnia", "Constipation"]),
+        (
+            [F.nmatches("name", r"^h.*s$")],
+            ["Hopelessness", "Helplessness", "Insomnia", "Constipation"],
+        ),
+        ([F.imatches("name", r"^h.*s$")], ["Hopelessness", "Helplessness"]),
+        ([F.nimatches("name", r"^h.*s$")], ["Insomnia", "Constipation"]),
+        ([F.in_("name", ("Insomnia", "Constipation"))], ["Insomnia", "Constipation"]),
+        (
+            [F.nin("name", ("Insomnia", "Constipation"))],
+            ["Hopelessness", "Helplessness"],
+        ),
+        ([F.lt("id", 2)], ["Hopelessness"]),
+        ([F.lte("id", 2)], ["Hopelessness", "Helplessness"]),
+        ([F.gt("id", 3)], ["Constipation"]),
+        ([F.gte("id", 3)], ["Insomnia", "Constipation"]),
+    ],
+)
+async def test_get_many_with_filter(
+    filter_: Sequence[F],
+    names: list[str],
+    symptoms_repo: SymptomsRepo,
+    hopelessness: Symptom,
+    helplessness: Symptom,
+    insomnia: Symptom,
+    constipation: Symptom,
+) -> None:
+    symptoms = await symptoms_repo.get_many(filter_=filter_)
+    assert [s.name for s in symptoms] == names
+
+
+@pytest.mark.parametrize(
+    "order, names",
+    [
+        (["name"], ["Constipation", "Helplessness", "Hopelessness", "Insomnia"]),
+        (["-name"], ["Insomnia", "Hopelessness", "Helplessness", "Constipation"]),
+        (
+            ("type", "-name"),
+            ["Insomnia", "Constipation", "Hopelessness", "Helplessness"],
+        ),
+        (
+            ("-type", "name"),
+            ["Helplessness", "Hopelessness", "Constipation", "Insomnia"],
+        ),
+    ],
+)
+async def test_get_many_with_order(
+    order: Sequence[str],
+    names: list[str],
+    symptoms_repo: SymptomsRepo,
+    hopelessness: Symptom,
+    helplessness: Symptom,
+    constipation: Symptom,
+    insomnia: Symptom,
+):
+    symptoms = await symptoms_repo.get_many(order=order)
+    assert [s.name for s in symptoms] == names
+
+
+@pytest.mark.parametrize(
+    "limit, page, names",
+    [
+        (2, 1, ["Hopelessness", "Helplessness"]),
+        (2, 2, ["Insomnia", "Constipation"]),
+        (3, 2, ["Constipation"]),
+    ],
+)
+async def test_get_many_with_page_and_limit(
+    limit: int,
+    page: int,
+    names: list[str],
+    symptoms_repo: SymptomsRepo,
+    hopelessness: Symptom,
+    helplessness: Symptom,
+    insomnia: Symptom,
+    constipation: Symptom,
+):
+    symptoms = await symptoms_repo.get_many(limit=limit, page=page)
+    assert [s.name for s in symptoms] == names
+
+
+async def test_delete(
+    symptoms_repo: SymptomsRepo, hopelessness: Symptom, helplessness: Symptom
+) -> None:
+    await symptoms_repo.delete(id=hopelessness.id)
+    symptoms = list(await symptoms_repo.get_many())
+    assert symptoms == [helplessness]
+
+
+async def test_update(symptoms_repo: SymptomsRepo, helplessness: Symptom) -> None:
+    helplessness.type = SymptomType.PHYSICAL
+    await symptoms_repo.update(helplessness)
+    symptom = await symptoms_repo.get(id=helplessness.id)
+    assert symptom == helplessness
+
+
+@pytest.mark.parametrize(
+    "params, result",
+    [
+        ({"name": "Hopelessness"}, True),
+        ({"name": "Euphoria"}, False),
+    ],
+)
+async def test_exists(
+    params: dict,
+    result: bool,
+    symptoms_repo: SymptomsRepo,
+    hopelessness: Symptom,
+) -> None:
+    assert await symptoms_repo.exists(**params) is result
+
+
+async def test_clean(
+    symptoms_repo: SymptomsRepo, hopelessness: Symptom, helplessness: Symptom
+) -> None:
+    await symptoms_repo.clean()
+    symptoms = list(await symptoms_repo.get_many())
+    assert symptoms == []
+
+
+async def test_count(
+    symptoms_repo: SymptomsRepo, hopelessness: Symptom, helplessness: Symptom
+) -> None:
+    assert await symptoms_repo.count() == 2
